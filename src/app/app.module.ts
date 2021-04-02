108e6fb8bfe396deec0441af921dcad699d48e2f
SAMADESA <SAMADESA@SAMADESA-T490.in.oracle.com>
2021-04-02 13:17:25 +0530

    All the files

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { UniquePipe } from './unique.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UniquePipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
